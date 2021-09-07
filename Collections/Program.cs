7cd2ffc78155e55e2afba41813327b08777de214
Fidel Makatia <fidel.makatia@autodesk.com>
2021-09-07 13:56:49 +0300

    first set of algorithims related to list

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -6,11 +6,56 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            List<int> myList = new List<int>(){10, 10, 11};
+            List<int> myList = new List<int>(10){10, 10, 11};
+            List<int> otherList = new List<int>() { 1000, 30, 56 };
+
+
+            //add method
+
+            myList.Add(400);
+
+            //add range method
+
+            myList.AddRange(otherList);
+
+            //insert
+
+            myList.Insert(1, 20);
+
+            //insert range
+
+           myList.InsertRange(3, otherList);
+
+
+
+
+            //Remove
+            myList.Remove(10);
+
+            //Remove AT an index
+            int ind = 0;
+            if (ind < myList.Count)
+            {
+                myList.RemoveAt(ind);
+            }
+            //read Elements
+            Console.WriteLine("Before remove range");
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Remove range
+
+            myList.RemoveRange(0, 3);
+
+            //remove all
+            myList.RemoveAll(n => n >= 60);// using lamda expression
+
 
             //read Elements
-
-            foreach(var item in myList)
+            Console.WriteLine("After remove range");
+            foreach (var item in myList)
             {
                 Console.WriteLine(item);
             }
