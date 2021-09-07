cf9b1829ea3353c78cff79398df8043e8eb17fd8
Fidel Makatia <fidel.makatia@autodesk.com>
2021-09-07 13:24:00 +0300

    Add project files.

---
+++ Collections/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>(){10, 10, 11};
+
+            //read Elements
+
+            foreach(var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
